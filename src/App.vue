c80d2421ddc38455f53664721dd211e34e50175f
AIR-DUSTER1 <1732027483@qq.com>
2024-09-26 07:47:24 +0800

    refactor(dev-scripts): 移除冗余构建与启动脚本 移除'build-only'和'type-check'脚本以简化构建过程。新增'build:env'脚本，允许在开发模式下进行构建。更新'dev'和'build'脚本，明确指定开发和生产模式。 refactor(App.vue): 添加环境变量日志 在App.vue中添加控制台日志以打印环境变量，增强环境配置的调试可访问性。 refactor(setting): 使用环境变量配置ApiAddress 在setting.ts中，将ApiAddress硬编码替换为从环境变量VITE_BASE_URL导入的值，提高配置的灵活性和环境管理能力。

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
     <RouterView></RouterView>
 </template>
 <script lang="ts" setup>
+console.log(import.meta.env);
 
 </script>
 <style lang="scss" scoped></style>
--- src/setting/setting.ts
+++ src/setting/setting.ts
@@ -1,4 +1,4 @@
 export const projectName = '飞鸟智慧校园生活服务平台'
-export const ApiAddress = 'http://110.40.67.194:45654'
+export const ApiAddress = import.meta.env.VITE_BASE_URL
 // export const ApiAddress = 'https://1f71b30a.r10.cpolar.top'
 
